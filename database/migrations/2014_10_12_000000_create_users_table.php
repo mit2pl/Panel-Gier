2d357d5ef7df0beb63080f245f2d653af08120eb
mit2 <mit2pl@gmail.com>
2021-01-15 17:36:28 +0100

    master

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,9 +14,11 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
+            $table->bigIncrements("id");
+            $table->string('login')->unique();
             $table->string('email')->unique();
+            $table->text('name');
+            $table->text('surname');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
