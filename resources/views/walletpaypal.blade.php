b5ad7d0cbae1ea95cae00f6787cf380a14d31c15
mit2 <mit2pl@gmail.com>
2021-02-09 15:57:30 +0100

    master

--- resources/views/walletpaypal.blade.php
+++ resources/views/walletpaypal.blade.php
@@ -3,8 +3,18 @@
 @section('body')
 <div id="paypalwlletmiddle">
     <div id="paypalwallettextup">
-        Płatność paypal
+        {{ __('Paypal Payment')}}
     </div>
-    <div id="paypalwalletrest">Tu będzie reszta</div>
+    <div id="paypalwalletrest">
+        <table style="margin: 0 auto;">
+            <tr style="margin-bottom:10px">
+                <td style="padding-right: 10px;text-transform: uppercase; font-weight: 400; font-size: 18px;">{{ __('Type the amount') }}</td>
+                <td><input class="inputseelogin" style="color: #c5c5c5;" type="number" name="kwotapaypal" placeholder="Wpisz Liczbę"></td>
+            </tr>
+            <tr>
+                <td style="padding-top: 20px;" colspan="2"><input class="btn btn-primary btnmytry" type="button" value="{{ __('Pay for this')}}"></td>
+            </tr>
+        </table>
+    </div>
 </div>
 @endsection
